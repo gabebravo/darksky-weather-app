2be1ddc67df9a5a090174347c712a1380fc81c89
Gabe Bravo <gabecbravo@gmail.com>
2020-02-28 14:33:57 -0500

    Made some small changes

--- src/containers/AddressContainer.js
+++ src/containers/AddressContainer.js
@@ -7,60 +7,81 @@ import Header from '../components/Header';
 import AddressSearch from '../components/AddressSearch';
 
 const defaultState = {
-  address: '', 
-  redirect: false, 
-  lat: '0', 
+  address: '',
+  redirect: false,
+  lat: '0',
   lng: '0'
-}
+};
 
 class AddressContainer extends Component {
-
   state = defaultState;
 
-  componentWillUnmount(){
+  componentWillUnmount() {
     this.setState(defaultState);
   }
 
-  updateAddress = (e) => {
+  updateAddress = e => {
     const address = e.target.value;
-    this.setState({ address })
-  }
+    this.setState({ address });
+  };
 
   getMapCoordinates = () => {
-    const GOOGLE_MAP_API = 'https://maps.googleapis.com/maps/api/geocode/json?address=';
-    if( this.state.address.length === 0 ) { return console.log('Please enter and address') };
+    const { REACT_APP_GOOGLE_MAP_API, REACT_APP_GM_KEY } = process.env;
+    if (this.state.address.length === 0) {
+      return console.log('Please enter and address');
+    }
     const addressString = encodeURIComponent(this.state.address); // formats string to query string url param
-    
-    axios.get(`${GOOGLE_MAP_API}${addressString}`)
-    .then(result => {
-      const {lat,lng} = result.data.results[0].geometry.location;
-      this.setState({ redirect: true, lat, lng });
-    })
-    .catch(response => {
-      if (response instanceof Error) {
-      // Something happened in setting up the request that triggered an Error 
-        console.log('Error:', 'That request cannot be processed. Please check the address.');
-      } else {
-        // The request was made, but the server responded with a status code that falls out of the range of 2xx 
-        console.log('Unable to connect to Google servers');
-      }
-    })
-  }
+
+    axios
+      .get(
+        `${REACT_APP_GOOGLE_MAP_API}${addressString}&key=${REACT_APP_GM_KEY}`
+      )
+      .then(result => {
+        const { lat, lng } = result.data.results[0].geometry.location;
+        this.setState({ redirect: true, lat, lng });
+      })
+      .catch(response => {
+        if (response instanceof Error) {
+          // Something happened in setting up the request that triggered an Error
+          console.log(
+            'Error:',
+            'That request cannot be processed. Please check the address.'
+          );
+        } else {
+          // The request was made, but the server responded with a status code that falls out of the range of 2xx
+          console.log('Unable to connect to Google servers');
+        }
+      });
+  };
 
   render() {
-    const redirect = this.state.redirect ? 
-      <Redirect push to={{ // push keyword required for back clicking
-        pathname: `/weather/${this.state.lat}/${this.state.lng}`,
-      }}/>: '';
+    const redirect = this.state.redirect ? (
+      <Redirect
+        push
+        to={{
+          // push keyword required for back clicking
+          pathname: `/weather/${this.state.lat}/${this.state.lng}`
+        }}
+      />
+    ) : (
+      ''
+    );
     return (
-      <Container>{redirect}
+      <Container>
+        {redirect}
         <Header />
-        <Row style={{marginTop: '5rem'}}>
+        <Row style={{ marginTop: '5rem' }}>
           <Col xs="12" sm="12" md={{ size: 8, offset: 2 }}>
-            <AddressSearch adressHandler={this.updateAddress}
-              addressValue={this.state.address}/>
-            <Button style={{ backgroundColor: '#fc41a1', color: '#fff'}}
-            onClick={this.getMapCoordinates}>Search</Button>
+            <AddressSearch
+              adressHandler={this.updateAddress}
+              addressValue={this.state.address}
+            />
+            <Button
+              style={{ backgroundColor: '#fc41a1', color: '#fff' }}
+              onClick={this.getMapCoordinates}
+            >
+              Search
+            </Button>
           </Col>
         </Row>
       </Container>
